bb193b21f5c2f8686e8dedb0740145b1fca74df0
Arya <agk8@uw.edu>
2023-05-15 18:51:53 -0700

    this time actually should be working

--- context-faithful-llm-main/generate_intersection.py
+++ context-faithful-llm-main/generate_intersection.py
@@ -1,5 +1,7 @@
 import json
 
+# enter dataset file paths here, make sure to match 1 and 2 for all 3 sets of fields.
+
 orig_dataset1 = "" 
 orig_dataset2 = ""
 
@@ -14,58 +16,67 @@ out_orig_path = f'./orig_dev_filtered_{engine1}_{engine2}.json'
 
 od1_uids = set()
 with open(orig_dataset1, "r") as od1:
-    for i, line in enumerate(od1):
-        datum = json.loads(line)
-        uid = datum['uid']
+    line = od1.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         od1_uids.add(uid)
 
 od2_uids = set()
 with open(orig_dataset2, "r") as od2:
-    for i, line in enumerate(od2):
-        datum = json.loads(line)
-        uid = datum['uid']
+    line = od2.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         od2_uids.add(uid)
 
-
-od_intersect = od1.intersection(od2)
+# print(type(od1))
+od_intersect = od1_uids.intersection(od2_uids)
+# od_intersect = od1_uids & od2
 
 orig_examples = []
-with open(orig_dataset1, 'r') as od1, with open(out_orig_path, 'w') as out_od:
-     for i, line in enumerate(od1):
-        datum = json.loads(line)
-        uid = datum['uid']
+with open(orig_dataset1, 'r') as od1, open(out_orig_path, 'w') as out_od:
+    line = od1.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         if uid in od_intersect:
-            orig_examples.add(datum)
+            orig_examples.append(data)
     
-    json.dump(orig_examples, out_od)
+    print(f'Found {len(orig_examples)} original examples in intersection')
+    json.dump(orig_examples, out_od)    
 
 
 
 counter1_uids = set()
 with open(counter_dataset1, "r") as cd1:
-    for i, line in enumerate(cd1):
-        datum = json.loads(line)
-        uid = datum['uid']
+    line = cd1.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         counter1_uids.add(uid)
 
 counter2_uids = set()
 with open(counter_dataset2, "r") as cd2:
-    for i, line in enumerate(cd2):
-        datum = json.loads(line)
-        uid = datum['uid']
+    line =  cd2.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         counter2_uids.add(uid)
 
 
-cd_intersect = cd1.intersection(cd2)
+cd_intersect = counter1_uids.intersection(counter2_uids)
 
 counter_examples = []
-with open(counter_dataset1, 'r') as cd1, with open(out_counter_path, 'w') as out_cd:
-     for i, line in enumerate(cd1):
-        datum = json.loads(line)
-        uid = datum['uid']
+with open(counter_dataset1, 'r') as cd1, open(out_counter_path, 'w') as out_cd:
+    line = cd1.readline()
+    datum = json.loads(line)
+    for data in datum:
+        uid = data['uid']
         if uid in cd_intersect:
-            counter_examples.add(datum)
-    
+            counter_examples.append(data)
+
+    print(f'Found {len(counter_examples)} counter examples in intersection')
     json.dump(counter_examples, out_cd)
             
             
